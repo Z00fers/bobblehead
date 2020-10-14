3810207572d8fd84796b936f039d64821fc2ead5
Z00fers <david.thilman@elev.ga.ntig.se>
2020-10-14 14:49:25 +0200

    mindre småfixar med email

--- resources/views/email/signup.blade.php
+++ resources/views/email/signup.blade.php
@@ -5,22 +5,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
     <style>
+        html, body {
+            width: 100%;
+            height: 100%;
+            overflow: hidden;
+        }
         body {
+            margin: 0;
+            display: flex;
             background-color: #122932;
             color: #779CAB;
+            box-shadow: inset 1px 2px 5px rgba(255, 255, 255, 0.2), inset -1px -2px 5px rgba(0, 0, 0, 0.2);
         }
-        
+        div {
+            width: 100%;
+            height: 100%;
+            padding: 1em;
+        }
     </style>
-    <title>Document</title>
+    <title>Tack för din anmälan!</title>
 </head>
 
 <body>
-    <h3>Tack för din anmälan!</h3>
-    <p>Vi kommer meddela dig vid eventuella produktsläpp framöver.</p>
-    <br />
-    <small>- oof Inc</small>
+    <div class="container d-flex justify-content-center align-items-center">
+        <h2 class="font-weight-bold"><u>Tack för din anmälan!</u></h2>
+        <p>Vi kommer meddela dig vid eventuella produktsläpp framöver.</p>
+        <br />
+        <small>- oof Inc</small>
+    </div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha384-ZvpUoO/+PpLXR1lu4jmpXWu80pZlYUAfxl5NsBMWOEPSjUn/6Z/hRTt8+pR6L4N2" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
